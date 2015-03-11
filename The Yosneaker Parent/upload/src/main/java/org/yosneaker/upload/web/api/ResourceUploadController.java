e48460fca7dc93942f489648c78bc19160db87cd
Dourm <461517142@qq.com>
2015-03-11 21:58:51 +0800

    添加picture.json接口

--- The Yosneaker Parent/upload/src/main/java/org/yosneaker/upload/web/api/ResourceUploadController.java
+++ The Yosneaker Parent/upload/src/main/java/org/yosneaker/upload/web/api/ResourceUploadController.java
@@ -23,6 +23,8 @@ import org.yosneaker.common.tools.i18n.Message;
 import org.yosneaker.common.tools.i18n.MessageUtils;
 import org.yosneaker.upload.services.ResourceService;
 
+import com.loopj.android.http.Base64;
+
 @Controller
 @RequestMapping("/resources")
 public class ResourceUploadController {
@@ -40,23 +42,45 @@ public class ResourceUploadController {
 	@ResponseBody
 	public Message<String> uploadImage(@RequestParam(required = false) MultipartFile image, HttpServletRequest request) {
 		try {
-			if (image == null) {
-				return MessageUtils.warn("upload.resources.empty");
-			}
 			String username = getLoginUserName(request);
+			String extension = null;
 			if (StringUtils.isEmpty(username)) {
 				return MessageUtils.warn("upload.resources.post.noSession");
 			}
-			String extension = FilenameUtils.getExtension(image.getOriginalFilename());
-			if (StringUtils.isEmpty(extension)) {
-				return MessageUtils.warn("upload.resources.delete.noExtention");
-			} 
-			Map<String, String> metadata = new HashMap<String, String>();
-			metadata.put(AUTHOR, username);
-			metadata.put(FILE_NAME, image.getOriginalFilename());
-			//byte[] imageData = imageService.resizeImage(image.getOriginalFilename(), image.getBytes(), imageGeometry, Gravity.Center);
-			String resourcePath = resourceService.uploadResource(extension, image.getBytes(), metadata);
-			return MessageUtils.success("upload.resources.post.success", resourcePath);
+			
+			if (image == null) {
+				request.setCharacterEncoding("utf-8");  
+		        String photo = request.getParameter("image");  
+		        if(photo == null)
+		        	return MessageUtils.error("upload.resources.empty");
+		        extension = request.getParameter("name");
+		        Map<String, String> metadata = new HashMap<String, String>();
+				metadata.put(AUTHOR, username);
+				metadata.put(FILE_NAME, extension);
+			    // 对base64数据进行解码 生成 字节数组，不能直接用Base64.decode（）；进行解密  
+	            byte[] photoimg = Base64.decode(photo,Base64.DEFAULT); 
+	            for (int i = 0; i < photoimg.length; ++i) {  
+	                if (photoimg[i] < 0) {  
+	                    // 调整异常数据  
+	                    photoimg[i] += 256;  
+	                }  
+	            }  
+				//byte[] imageData = imageService.resizeImage(image.getOriginalFilename(), image.getBytes(), imageGeometry, Gravity.Center);
+				String resourcePath = resourceService.uploadResource(extension,photoimg, metadata);
+				return MessageUtils.success("upload.resources.post.success", resourcePath);
+		        
+			}else{
+				extension = FilenameUtils.getExtension(image.getOriginalFilename());
+				if (StringUtils.isEmpty(extension)) {
+					return MessageUtils.warn("upload.resources.delete.noExtention");
+				} 
+				Map<String, String> metadata = new HashMap<String, String>();
+				metadata.put(AUTHOR, username);
+				metadata.put(FILE_NAME, image.getOriginalFilename());
+				//byte[] imageData = imageService.resizeImage(image.getOriginalFilename(), image.getBytes(), imageGeometry, Gravity.Center);
+				String resourcePath = resourceService.uploadResource(extension, image.getBytes(), metadata);
+				return MessageUtils.success("upload.resources.post.success", resourcePath);
+			}
 		} catch (Exception e) {
 			LOGGER.error(e.getMessage(), e);
 			return MessageUtils.error("upload.resources.post.error");
@@ -116,5 +140,75 @@ public class ResourceUploadController {
 	
 	private String getLoginUserName(HttpServletRequest request) {
 		return "daniellitoc";
+	}
+	
+	@RequestMapping(method = { RequestMethod.POST }, value = { "/picture" })
+	@ResponseBody
+	public Message<String> uploadImage2(HttpServletRequest request) {
+		try {
+			String username = getLoginUserName(request);
+			String extension = "";
+			if (StringUtils.isEmpty(username)) {
+				return MessageUtils.warn("upload.resources.post.noSession");
+			}
+			
+				request.setCharacterEncoding("utf-8");  
+		        String photo = request.getParameter("image");  
+		        if(photo == null)
+		        	return MessageUtils.error("upload.resources.empty");
+		        extension = request.getParameter("name");
+		        Map<String, String> metadata = new HashMap<String, String>();
+				metadata.put(AUTHOR, username);
+				metadata.put(FILE_NAME, extension);
+			    // 对base64数据进行解码 生成 字节数组，不能直接用Base64.decode（）；进行解密  
+	            byte[] photoimg = Base64.decode(photo,Base64.DEFAULT); 
+	            for (int i = 0; i < photoimg.length; ++i) {  
+	                if (photoimg[i] < 0) {  
+	                    // 调整异常数据  
+	                    photoimg[i] += 256;  
+	                }  
+	            }  
+				//byte[] imageData = imageService.resizeImage(image.getOriginalFilename(), image.getBytes(), imageGeometry, Gravity.Center);
+				String resourcePath = resourceService.uploadResource(extension,photoimg, metadata);
+				return MessageUtils.success("upload.resources.post.success", resourcePath);
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage(), e);
+			return MessageUtils.error("upload.resources.post.error");
+		}
+	}
+	
+	@RequestMapping(method = { RequestMethod.POST }, value = { "/picture.json" })
+	@ResponseBody
+	public Message<String> uploadImage3(HttpServletRequest request) {
+		try {
+			String username = getLoginUserName(request);
+			String extension = "";
+			if (StringUtils.isEmpty(username)) {
+				return MessageUtils.warn("upload.resources.post.noSession");
+			}
+			
+				request.setCharacterEncoding("utf-8");  
+		        String photo = request.getParameter("image");  
+		        if(photo == null)
+		        	return MessageUtils.error("upload.resources.empty");
+		        extension = request.getParameter("name");
+		        Map<String, String> metadata = new HashMap<String, String>();
+				metadata.put(AUTHOR, username);
+				metadata.put(FILE_NAME, extension);
+			    // 对base64数据进行解码 生成 字节数组，不能直接用Base64.decode（）；进行解密  
+	            byte[] photoimg = Base64.decode(photo,Base64.DEFAULT); 
+	            for (int i = 0; i < photoimg.length; ++i) {  
+	                if (photoimg[i] < 0) {  
+	                    // 调整异常数据  
+	                    photoimg[i] += 256;  
+	                }  
+	            }  
+				//byte[] imageData = imageService.resizeImage(image.getOriginalFilename(), image.getBytes(), imageGeometry, Gravity.Center);
+				String resourcePath = resourceService.uploadResource(extension,photoimg, metadata);
+				return MessageUtils.success("upload.resources.post.success", resourcePath);
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage(), e);
+			return MessageUtils.error("upload.resources.post.error",e);
+		}
 	}
 }
